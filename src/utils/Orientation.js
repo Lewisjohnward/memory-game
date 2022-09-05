b5942785a4d7cb244424cf8662b75783c78f0b0c
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2022-09-05 09:12:28 +0100

    orientation

--- src/utils/Orientation.js
+++ src/utils/Orientation.js
@@ -1,56 +1,11 @@
 import {useState, useEffect} from "react"
-import styled from "styled-components"
-
-const Container = styled.div`
-    position: absolute;
-    right: 0;
-    display: flex;
-    opacity: 0.6;
-
-    > * {
-        margin-right: 5px;
-    }
-`
 
 const Orientation = () => {
-
-    const [dimensions, setDimensions] = useState({
-        width: window.innerWidth,
-        height: window.innerHeight
-    })
-
     useEffect(() => {
-        const handleResize = () => {
-            const width = window.innerWidth
-            const height = window.innerHeight
-            setDimensions({
-                width,
-                height
-            })
-            console.log(`Resized to: ${width}, ${height}`)
-        }
+        const handleOrientationChange = () => console.log(window.orientation)
 
-        window.addEventListener('resize', handleResize)
+        window.addEventListener('orientationchange', handleOrientationChange)
     }, [])
-
-    const device = () => dimensions.width <= 320 ? "mobile" : dimensions.width <= 768 ? "Tablet" : "Desktop"
-
-    return (
-        <Container>
-            <div>
-                {dimensions.width}
-            </div>
-            <div>
-                x
-            </div>
-            <div>
-                {dimensions.height}
-            </div>
-            <div>
-                {device()}
-            </div>
-        </Container>
-    )
 }
 
-export default Size
+export default Orientation
